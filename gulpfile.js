a415166f8d876ce225377be9628f1c86bef5cec4
Arthur Ming <guobin.ming@pwc.com>
2020-03-16 23:30:48 +0800

    build: add rollup build support

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 const { series, src, dest } = require('gulp');
+const { rollup } = require('rollup');
+const sourcemap = require('rollup-plugin-sourcemaps');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+
 const del = require('delete');
 const ts = require('gulp-typescript');
 var merge = require('merge2');
 const tsProject = ts.createProject('tsconfig.json');
 
-// `clean` 函数并未被导出（export），因此被认为是私有任务（private task）。
-// 它仍然可以被用在 `series()` 组合中。
 function clean(cb) {
     // body omitted
     del(['dist'], cb);
 }
 
-// `build` 函数被导出（export）了，因此它是一个公开任务（public task），并且可以被 `gulp` 命令直接调用。
-// 它也仍然可以被用在 `series()` 组合中。
 function move(cb) {
     return src('src/**/*.ts')
         .pipe(dest('dist/src'))
@@ -23,10 +24,33 @@ function build(cb) {
         .src()
         .pipe(tsProject());
     return merge([
-            compiler.js.pipe((dest('dist/src'))),
-            compiler.dts.pipe((dest('dist/src')))
-        ]);
+        compiler.js.pipe((dest('dist/src'))),
+        compiler.dts.pipe((dest('dist/src')))
+    ]);
+}
+
+async function buildByRollup(cd) {
+    const bundle = await rollup({
+        input: 'dist/src/index.js',
+        plugins: [
+            sourcemap(),
+            resolve({
+                jsnext: true,
+                main: true,
+                browser: false
+              }),
+            commonjs()
+        ]
+    });
+    return bundle.write({
+        file: 'dist/knexer.js',
+        format: 'umd',
+        name: 'knexer',
+        sourcemap: true,
+        exports: 'named',
+        amd: { id: 'knexer' }
+    });
 }
 
 exports.build = build;
-exports.default = series(clean, move, build)
+exports.default = series(clean, move, build, buildByRollup)
