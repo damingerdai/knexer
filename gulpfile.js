d66734b878c77c8d6a9fdcd7576e16dd2da55384
Arthur Ming <guobin.ming@pwc.com>
2020-04-12 23:04:08 +0800

    ci: rewirte the gulp building

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { series, src, dest } = require('gulp');
+const { series, src, dest, parallel} = require('gulp');
 const { rollup } = require('rollup');
 const sourcemap = require('rollup-plugin-sourcemaps');
 const resolve = require('rollup-plugin-node-resolve');
@@ -10,46 +10,52 @@ var merge = require('merge2');
 const tsProject = ts.createProject('tsconfig.json');
 
 function clean(cb) {
-    // body omitted
-    del(['dist'], cb);
+  // body omitted
+  del(['dist'], cb);
 }
 
-function move(cb) {
-    return src('src/**/*?.ts')
-        .pipe(dest('dist/src'))
+function moveSourceFile() {
+  return src('src/**/*?.ts')
+    .pipe(dest('dist/src'))
+
 }
 
-function build(cb) {
-    const compiler = tsProject
-        .src()
-        .pipe(tsProject());
-    return merge([
-        compiler.js.pipe((dest('dist/src'))),
-        compiler.dts.pipe((dest('dist/src')))
-    ]);
+function moveResourceFile() {
+  return src(['*.json', '*.md', 'LICENSE'])
+    .pipe(dest('dist/'))
+}
+
+function buildTypescript(cb) {
+  const compiler = tsProject
+    .src()
+    .pipe(tsProject());
+  return merge([
+    compiler.js.pipe((dest('dist/src'))),
+    compiler.dts.pipe((dest('dist/src')))
+  ]);
 }
 
 async function buildByRollup(cd) {
-    const bundle = await rollup({
-        input: 'dist/src/index.js',
-        plugins: [
-            sourcemap(),
-            resolve({
-                jsnext: true,
-                main: true,
-                browser: false
-              }),
-            commonjs()
-        ]
-    });
-    return bundle.write({
-        file: 'dist/knexer.js',
-        format: 'umd',
-        name: 'knexer',
-        sourcemap: true,
-        exports: 'named',
-        amd: { id: 'knexer' }
-    });
+  const bundle = await rollup({
+    input: __dirname + '/dist/src/knexer.js',
+    plugins: [
+      sourcemap(),
+      resolve({
+        jsnext: true,
+        main: true,
+        browser: false
+      }),
+      commonjs()
+    ]
+  });
+  return bundle.write({
+    file: 'dist/knexer.js',
+    format: 'umd',
+    name: 'knexer',
+    sourcemap: true,
+    exports: 'named',
+    amd: { id: 'knexer' }
+  });
 }
 
 function cleanDist(cb) {
@@ -57,5 +63,5 @@ function cleanDist(cb) {
   del(['dist/**/__tests__/**'], cb);
 }
 
-exports.build = build;
-exports.default = series(clean, move, build, buildByRollup, cleanDist)
+/// exports.build = build;
+exports.default = series(clean, parallel(moveSourceFile, moveResourceFile), series(buildTypescript, buildByRollup), cleanDist)
